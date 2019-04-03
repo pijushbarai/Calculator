bf1778981e27fca6f70f11126a326b46fefa2efe
Sajeeb Chakraborty <sajeebchakraborty.cse2000@gmail.com>
2019-04-03 10:39:14 +0600

    Multiplication

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,15 @@
 public class Calculator{
     public static void main(String args[]){
         
-    }
+	public int multiplicationMethod(int Firstvalue,int Secondvalue)
+	{
+	
+		int result;
+		result =(Firstvalue*Secondvalue);
+		return result;
+
+
+
+	}
+		}
 }
