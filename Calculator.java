f84a4ed14b838c0a6bf787e752e81fe5f5c75e32
Jahid Hasan <jahidcseru1998@gmail.com>
2019-04-03 10:45:23 +0600

    Subtraction Method added to main Method

--- Calculator.java
+++ Calculator.java
@@ -2,4 +2,9 @@ public class Calculator{
     public static void main(String args[]){
         
     }
+	public int subtractionMethod(int First_value,Second_value)
+	{
+		int Result;
+		Result=First_value-Second_value;
+		return Result;
 }
